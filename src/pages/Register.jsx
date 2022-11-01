6de2640b0537e56371357ac84a93f37d7dcc7aa9
Kemal <ttnckemal@gmail.com>
2022-11-01 21:18:40 +0100

    commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -11,7 +11,7 @@ import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { useState } from 'react'
+
 import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 
