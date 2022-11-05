dfe856eb0089916539413822511efe44521894a9
Kemal <ttnckemal@gmail.com>
2022-11-06 00:08:18 +0100

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 // import css from '../css/Home.css'
 import Navbar from '../components/Navbar'
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 import Loading from '../components/Loading'
 // import Login from '../components/Login'
 import Copyright from '../components/Copyright'
 import Login from '../pages/Login'
 export default function Home({ isLogin, setIsLogin, loading, setLoading }) {
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
   return (
     <>
       <Navbar
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { useNavigate, Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const theme = createTheme()
 
